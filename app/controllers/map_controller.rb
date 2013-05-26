414a99b26e297a4ec5add52a123539b04845b347
Marcus Gartner <marcus@fanium.com>
2013-05-26 15:43:50 -0700

    added locations

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,20 +1,35 @@
 class MapController < UIViewController
 
+  # TODO: Fix Acai and D Town names.
+  LOCATIONS = [{name: "Leilani's Cafe", lat: 32.80795, long: -117.25491},
+               {name: "Java Earth Cafe", lat: 32.80550, long: -117.25448},
+               {name: "Acai?", lat: 32.79977, long: -117.25258},
+               {name: "Te Mana Cafe", lat: 32.75178, long: -117.24572},
+               {name: "D Town", lat: 32.71744, long: 	-117.16239}]
+
   def viewDidLoad
+    super
 
     # Create a GMSCameraPosition that tells the map to display the
     # coordinate -33.86,151.20 at zoom level 6.
-    camera = GMSCameraPosition.cameraWithLatitude(-33.86, longitude: 151.20, zoom: 6)
-    map_view = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
-    #map_view.myLocationEnabled = true
-    self.view = map_view
+    camera = GMSCameraPosition.cameraWithLatitude(32.75, longitude: -117.2, zoom: 11)
+    @map_view = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+    @map_view.myLocationEnabled = true
+    self.view = @map_view
 
-    # Create a marker in the center of the map.
-    marker = GMSMarker.alloc.init
-    marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
-    marker.title = "Sydney"
-    marker.snippet = "Australia"
-    marker.map = map_view
+  end
+
+  def viewWillAppear(animated)
+    super
+
+    LOCATIONS.each do |location|
+      marker = GMSMarker.alloc.init
+      marker.position = CLLocationCoordinate2DMake(location[:lat], location[:long])
+      marker.title = location[:name]
+      # marker.snippet = location[:snippet]
+      marker.map = @map_view
+    end
+
   end
 
 end
